4371730fbf250cec1014bf41e4bb63e89b84a052
lsy1205 <stevenliu901205@gmail.com>
2024-12-17 01:58:46 +0800

    add oled and max30102

--- Arduino/Final.ino
+++ Arduino/Final.ino
@@ -1,7 +1,12 @@
 #include <ArduinoBLE.h>
 #include <ACROBOTIC_SSD1306.h>
+
 #include "Arduino_BMI270_BMM150.h"
+
 #include <Wire.h>
+
+#include "MAX30105.h"
+#include "heartRate.h"
 
 // Define Time Interval
 unsigned long currentMillis = 0;
@@ -15,6 +20,19 @@ int recordPin = D3;
 bool buttonRecord = true;
 bool preButtonRecord = true;
 
+// MAX30102
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
+
+char formattedBeat[4]; // 用於存儲格式化後的字串，3 位數字 + 結束符號 '\0'
+
 // Define BLE and UUID
 BLEService imuService("12345678-1234-1234-1234-123456789abc"); // Define UUID
 BLECharacteristic imuCharacteristic("87654321-4321-4321-4321-abcde1234567", BLERead | BLEWrite | BLENotify, 512); // 24 字节用于发送 6 个浮点数
@@ -27,18 +45,31 @@ void setup() {
   if (Serial) {
     Serial.println("Started");
   }
+
+  // OLED
   Wire.begin();	
   oled.init();                      // Initialze SSD1306 OLED display
   oled.clearDisplay();              // Clear screen
   oled.setTextXY(0,0);
   oled.putString("BurnSync");
-  oled.setTextXY(2,0);
+  oled.setTextXY(1,0);
   oled.putString("Fitness Tracker");
-  oled.setTextXY(4,0);
-  oled.putString("Status: ");
-  oled.setTextXY(5,0);
+  
+  oled.setTextXY(3,0);
   oled.putString("Disconnected");
   
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println("MAX30105 was not found. Please check wiring/power. ");
+    while (1);
+  }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
+
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+
   // Pin Mode
   pinMode(recordPin, INPUT_PULLUP);
   digitalWrite(recordPin, HIGH);
@@ -80,11 +111,17 @@ void loop() {
       Serial.println(central.address());
     }
     if (central.connected()) {
-        oled.setTextXY(5,0);
+        oled.setTextXY(3,0);
         oled.putString("Connected   ");
+        oled.setTextXY(6,0);
+        oled.putString("Heart Rate ");
+        oled.setTextXY(7,0);
+        oled.putString("Avg BPM: ");
     }
     while (central.connected()) {
         currentMillis = millis();
+
+        // IMU Data
         if (currentMillis - previousMillisBLE >= intervalBLE) {
             previousMillisBLE = currentMillis;
 
@@ -125,16 +162,70 @@ void loop() {
         // Record Button
         buttonRecord = digitalRead(recordPin);
         
-        // if(Serial){
-        //     Serial.println(buttonRecord);
-        // }
         if (buttonRecord == 1 && preButtonRecord == 0) {
             record = !record;
         }
         preButtonRecord = buttonRecord;
+        if (record) {
+            oled.setTextXY(4,0);
+            oled.putString("Recording    ");
+        } else {
+            oled.setTextXY(4,0);
+            oled.putString("Not Recording");
+        }
+
+        // Heart Rate
+        long irValue = particleSensor.getIR();
+
+        if (checkForBeat(irValue) == true)
+        {
+          Serial.println("Got new data!");
+          //We sensed a beat!
+          long delta = millis() - lastBeat;
+          lastBeat = millis();
+
+          beatsPerMinute = 60 / (delta / 1000.0);
+
+          if (beatsPerMinute < 150 && beatsPerMinute > 50)
+          {
+            rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+            rateSpot %= RATE_SIZE; //Wrap variable
+
+            //Take average of readings
+            beatAvg = 0;
+            for (byte x = 0 ; x < RATE_SIZE ; x++)
+              beatAvg += rates[x];
+            beatAvg /= RATE_SIZE;
+          }
+        }
+
+        if (Serial) {
+          Serial.print("IR=");
+          Serial.print(irValue);
+          Serial.print(", BPM=");
+          Serial.print(beatsPerMinute);
+          Serial.print(", Avg BPM=");
+          Serial.println(beatAvg);
+        }
+
+        if (irValue < 50000){
+          if (Serial) {
+            Serial.println("No finger?");
+          }
+          oled.setTextXY(7,10);
+          oled.putString("???");
+        } else {
+          oled.setTextXY(7,10);
+          sprintf(formattedBeat, "%3d", beatAvg);
+          oled.putString(formattedBeat);
+        }
     }
-    oled.setTextXY(5,0);
+    oled.setTextXY(3,0);
     oled.putString("Disconnected");
+    oled.setTextXY(6,0);
+    oled.putString("           ");
+    oled.setTextXY(7,0);
+    oled.putString("             ");
 
     if (Serial) {
       Serial.println("Disconnected from central");
